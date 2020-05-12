79cad6da17963f98048a123d361afa4d0f96f5c5
zhoupanwei <pstzpw@163.com>
2020-05-12 14:07:57 +0800

    最新

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -1,42 +1,46 @@
 package com.coolweather.android.db;
 
-import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
 
-public class City extends DataSupport {
-    private int id;
-    private String cityName;
-    private int cityCode;
-    private int provinceCode;
-
-    public int getId() {
-        return id;
-    }
+public class City extends LitePalSupport {
+    int id;
+    String cityName;
+    int cityCode;
+    int provinceCode;
+    int provinceId;
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getCityName() {
-        return cityName;
     }
 
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
 
-    public int getCityCode() {
-        return cityCode;
-    }
-
     public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
     }
 
     public int getProvinceCode() {
         return provinceCode;
     }
 
-    public void setProvinceCode(int provinceCode) {
-        this.provinceCode = provinceCode;
+    public void setProvinceId(int provinceId) {this.provinceId = provinceId;
     }
 }
--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -1,46 +1,42 @@
 package com.coolweather.android.db;
 
-import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
 
-public class County extends DataSupport {
-    private int id;
-    private String countyName;
-    private String weatherId;
-    private int cityId;
-
-
-    public int getId() {
-        return id;
-    }
+public class County extends LitePalSupport {
+    int id;
+    String countyName;
+    String weatherId;
+    int cityId;
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getWeatherId() {
-        return weatherId;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
 
-    public int getCityId() {
-        return cityId;
-    }
-
     public void setCityId(int cityId) {
         this.cityId = cityId;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getCountyName() {
+        return countyName;
+    }
 
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
 }
